804af9e274e6897dc32aa852591a46e5ddc04715
yatsun <yatsunlam25@gmail.com>
2021-03-06 19:07:41 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -100,13 +100,22 @@ def changing_alan
         }        
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-    alans_new_info = "GUI"  
-    programmer_hash = {
-    :alan_kay => {
-      :known_for => alans_new_info,
-      :languages => ["Smalltalk", "LISP"]
-    }
-  }
+ #   alans_new_info = "GUI"  
+  #  programmer_hash = {
+  #  :alan_kay => {
+  #    :known_for => alans_new_info,
+  #    :languages => ["Smalltalk", "LISP"]
+ #   }
+ # }
+  programmer_hash.each do |name, value|
+    if name == alan_kay
+      name.collect do |key, value2| 
+        if key == languages
+          value2 = alans_new_info = "GUI"
+        end
+      end
+    end
+  end 
 end
 
 def adding_to_dennis

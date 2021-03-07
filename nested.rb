11ef3950d9cbf7be3c995e15bd0f158d7fa28465
yatsun <yatsunlam25@gmail.com>
2021-03-06 19:36:37 -0500

    commit tree bb708ee25a1f09b0f42d877cde07e2444b060cad

--- nested.rb
+++ nested.rb
@@ -102,13 +102,12 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
     alans_new_info = "GUI"  
     programmer_hash = {
-    :alan_kay => {
-      :known_for => alans_new_info,
+     :alan_kay => {
+     :known_for => alans_new_info,
       :languages => ["Smalltalk", "LISP"]
     }
   }
-
-
+  
 end
 
 def adding_to_dennis
